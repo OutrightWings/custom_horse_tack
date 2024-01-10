576d4952fc4ab1cbff3dc57d268f9e579a0e8a55
lilym <lilymason@msn.com>
2024-01-10 02:07:43 -0800

    missing stat stick

--- src/main/java/com/outrightwings/truly_custom_horse_tack/item/ModItems.java
+++ src/main/java/com/outrightwings/truly_custom_horse_tack/item/ModItems.java
@@ -19,7 +19,7 @@ public class ModItems {
     public static final RegistryObject<StatPotion> DECREASE_JUMP_POTION = ITEMS.register("decrease_jump_potion",() -> new StatPotion(new Item.Properties().tab(ModCreativeTab.instance),false,true));
     public static final RegistryObject<StatPotion> INCREASE_SPEED_POTION = ITEMS.register("increase_speed_potion",() -> new StatPotion(new Item.Properties().tab(ModCreativeTab.instance),true,false));
     public static final RegistryObject<StatPotion> DECREASE_SPEED_POTION = ITEMS.register("decrease_speed_potion",() -> new StatPotion(new Item.Properties().tab(ModCreativeTab.instance),false,false));
-    public static final RegistryObject<HorseStick> HORSE_STICK = ITEMS.register("horse_stick",()->new HorseStick(new Item.Properties()));
+    public static final RegistryObject<HorseStick> HORSE_STICK = ITEMS.register("horse_stick",()->new HorseStick(new Item.Properties().tab(ModCreativeTab.instance)));
 
     public static final RegistryObject<HorseArmorItem> CUSTOM_TACK_ITEM = registerHorseArmor("custom_tack",3);
     public static final RegistryObject<Item> WINTER_TACK_PATTERN = ITEMS.register("winter_tack_pattern",() -> new TackPatternItem("winter",(new Item.Properties()).stacksTo(1).tab(ModCreativeTab.instance)));
