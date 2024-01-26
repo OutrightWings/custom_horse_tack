f94cecf5b2c13d9d23c0a46741401ca6dc627d0f
lilym <lilymason@msn.com>
2024-01-26 12:25:05 -0800

    Starting of capabilities

--- src/main/java/com/outrightwings/truly_custom_horse_tack/block/HeadStand.java
+++ src/main/java/com/outrightwings/truly_custom_horse_tack/block/HeadStand.java
@@ -1,17 +1,58 @@
 package com.outrightwings.truly_custom_horse_tack.block;
 
+import com.outrightwings.truly_custom_horse_tack.block.entity.HeadStandBlockEntity;
 import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.Container;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.ContainerData;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.ForgeCapabilities;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.network.NetworkHooks;
 import org.jetbrains.annotations.Nullable;
 
 public class HeadStand extends BaseEntityBlock {
-
+    final ContainerData data;
     public HeadStand(Properties properties) {
         super(properties);
+        this.data = new ContainerData() {
+            @Override
+            public int get(int index) {
+                return 0;
+            }
+
+            @Override
+            public void set(int index, int value) {
+
+            }
+
+            @Override
+            public int getCount() {
+                return 1;
+            }
+        };
+    }
+
+
+    @Override
+    public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
+        if (!level.isClientSide) {
+            BlockEntity entity = level.getBlockEntity(pos);
+            if(entity instanceof HeadStandBlockEntity stand){
+
+            }
+        }
+        return InteractionResult.sidedSuccess(level.isClientSide);
     }
 
     @Nullable
