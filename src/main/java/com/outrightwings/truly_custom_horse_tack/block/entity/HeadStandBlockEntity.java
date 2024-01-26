f118b3aa0b9b288ab9d76b6d87235f586b0614f6
lilym <lilymason@msn.com>
2024-01-26 12:25:05 -0800

    Starting of capabilities

--- src/main/java/com/outrightwings/truly_custom_horse_tack/block/entity/HeadStandBlockEntity.java
+++ src/main/java/com/outrightwings/truly_custom_horse_tack/block/entity/HeadStandBlockEntity.java
@@ -1,11 +1,28 @@
 package com.outrightwings.truly_custom_horse_tack.block.entity;
 
 import com.outrightwings.truly_custom_horse_tack.block.ModBlocks;
+import com.outrightwings.truly_custom_horse_tack.item.CustomTackItem;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.items.ItemStackHandler;
 
 public class HeadStandBlockEntity extends BlockEntity {
+    private final ItemStackHandler inventory = new ItemStackHandler(1){
+        @Override
+        protected void onContentsChanged(int slot) {
+            setChanged();
+            if(!level.isClientSide()) {
+
+            }
+        }
+
+        @Override
+        public boolean isItemValid(int slot, ItemStack stack) {
+            return stack.getItem() instanceof CustomTackItem;
+        }
+    };
     public HeadStandBlockEntity(BlockPos pos, BlockState state) {
         super(ModBlocks.HEAD_STAND_BE.get(), pos, state);
     }
