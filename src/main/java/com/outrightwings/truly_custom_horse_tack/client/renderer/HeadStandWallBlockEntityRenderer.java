9e1ee4d0a51078b26db3548bd0c0f49eabd9aad8
lilym <lilymason@msn.com>
2024-01-30 03:58:12 -0800

    Only in client

--- src/main/java/com/outrightwings/truly_custom_horse_tack/client/renderer/HeadStandWallBlockEntityRenderer.java
+++ src/main/java/com/outrightwings/truly_custom_horse_tack/client/renderer/HeadStandWallBlockEntityRenderer.java
@@ -4,7 +4,10 @@ import com.outrightwings.truly_custom_horse_tack.Main;
 import com.outrightwings.truly_custom_horse_tack.client.renderer.model.HeadStandWallModel;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
+@OnlyIn(Dist.CLIENT)
 public class HeadStandWallBlockEntityRenderer extends DisplayModelEntityRenderer{
     public HeadStandWallBlockEntityRenderer(BlockEntityRendererProvider.Context context){
         this.displayModel = new HeadStandWallModel(HeadStandWallModel.createBodyLayer().bakeRoot());
