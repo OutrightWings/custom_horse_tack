108b4b2a95ac784ed508ac9a67a91996016864b0
lilym <lilymason@msn.com>
2024-01-30 01:06:16 -0800

    Collision on Ground Versions

--- src/main/java/com/outrightwings/truly_custom_horse_tack/block/HeadStand.java
+++ src/main/java/com/outrightwings/truly_custom_horse_tack/block/HeadStand.java
@@ -1,8 +1,10 @@
 package com.outrightwings.truly_custom_horse_tack.block;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.util.Mth;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Mirror;
 import net.minecraft.world.level.block.Rotation;
@@ -11,15 +13,24 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.Nullable;
 
 public class HeadStand extends SingleInventoryBlock {
     public static final IntegerProperty ROTATION = BlockStateProperties.ROTATION_16;
+    private static final VoxelShape BASE = Block.box(0,0,0,16,1,16);
+    private static final VoxelShape POLE = Block.box(4.5,0,4.5,11.5,12,11.5);
+    private static final VoxelShape BOUNDING = Shapes.or(BASE,POLE);
     public HeadStand(Properties properties) {
         super(properties);
         this.registerDefaultState(this.stateDefinition.any().setValue(ROTATION, 0));
-    }
 
+    }
+    public VoxelShape getShape(BlockState p_48816_, BlockGetter p_48817_, BlockPos p_48818_, CollisionContext p_48819_) {
+        return BOUNDING;
+    }
     @Override
     public float getRotation(BlockState state) {
         return state.getValue(ROTATION) * 360f / 16f;
