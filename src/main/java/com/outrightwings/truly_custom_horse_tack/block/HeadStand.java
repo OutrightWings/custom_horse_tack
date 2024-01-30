80bcf183eebde7b4d4243dd3aa43a4aacc61b006
lilym <lilymason@msn.com>
2024-01-30 00:17:22 -0800

    rotation

--- src/main/java/com/outrightwings/truly_custom_horse_tack/block/HeadStand.java
+++ src/main/java/com/outrightwings/truly_custom_horse_tack/block/HeadStand.java
@@ -1,18 +1,49 @@
 package com.outrightwings.truly_custom_horse_tack.block;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.util.Mth;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Mirror;
+import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import org.jetbrains.annotations.Nullable;
 
 public class HeadStand extends SingleInventoryBlock {
+    public static final IntegerProperty ROTATION = BlockStateProperties.ROTATION_16;
     public HeadStand(Properties properties) {
         super(properties);
+        this.registerDefaultState(this.stateDefinition.any().setValue(ROTATION, 0));
+    }
+
+    @Override
+    public float getRotation(BlockState state) {
+        return state.getValue(ROTATION) * 360f / 16f;
     }
 
     @Nullable
     @Override
     public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
         return ModBlocks.HEAD_STAND_BE.get().create(pos,state);
+    }
+
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return this.defaultBlockState().setValue(ROTATION, Mth.floor((double) (context.getRotation() * 16.0F / 360.0F) + 0.5D) & 15);
+    }
+
+    public BlockState rotate(BlockState state, Rotation rotation) {
+        return state.setValue(ROTATION, rotation.rotate(state.getValue(ROTATION), 16));
+    }
+
+    public BlockState mirror(BlockState state, Mirror mirror) {
+        return state.setValue(ROTATION, mirror.mirror(state.getValue(ROTATION), 16));
+    }
+
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(ROTATION);
     }
 }
--- src/main/java/com/outrightwings/truly_custom_horse_tack/block/SaddleRack.java
+++ src/main/java/com/outrightwings/truly_custom_horse_tack/block/SaddleRack.java
@@ -1,13 +1,49 @@
 package com.outrightwings.truly_custom_horse_tack.block;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
+import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import org.jetbrains.annotations.Nullable;
 
 public class SaddleRack extends SingleInventoryBlock {
+    public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
     public SaddleRack(Properties properties) {
         super(properties);
+        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
+    }
+
+    @Override
+    public float getRotation(BlockState state) {
+        Direction direction = state.getValue(FACING).getClockWise();
+        float angle;
+        if (direction == Direction.NORTH) {
+            angle = 90;
+        }else if(direction == Direction.EAST){
+            angle = 180;
+        }else if(direction == Direction.SOUTH){
+            angle = 270;
+        }else{
+            angle = 0;
+        }
+
+        return angle;
+    }
+
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getClockWise());
+    }
+    public BlockState rotate(BlockState state, Rotation rotation) {
+        return state.setValue(FACING, rotation.rotate(state.getValue(FACING)));
+    }
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
     }
     @Nullable
     @Override
