866ab6eddb0b664204881b2206ee9788eade2568
lilym <lilymason@msn.com>
2024-01-29 20:39:28 -0800

    Inventory Works and displays on Display Stands

--- src/main/java/com/outrightwings/truly_custom_horse_tack/block/entity/HeadStandBlockEntity.java
+++ src/main/java/com/outrightwings/truly_custom_horse_tack/block/entity/HeadStandBlockEntity.java
@@ -1,11 +1,17 @@
 package com.outrightwings.truly_custom_horse_tack.block.entity;
 
 import com.outrightwings.truly_custom_horse_tack.block.ModBlocks;
+import com.outrightwings.truly_custom_horse_tack.item.CustomTackItem;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class HeadStandBlockEntity extends SingleInventoryBlockEntity {
     public HeadStandBlockEntity(BlockPos pos, BlockState state) {
         super(ModBlocks.HEAD_STAND_BE.get(), pos, state);
+    }
+    @Override
+    public boolean validItem(ItemStack item) {
+        return item.getItem() instanceof CustomTackItem;
     }
 }
--- src/main/java/com/outrightwings/truly_custom_horse_tack/block/entity/SaddlerRackBlockEntity.java
+++ src/main/java/com/outrightwings/truly_custom_horse_tack/block/entity/SaddlerRackBlockEntity.java
@@ -1,11 +1,18 @@
 package com.outrightwings.truly_custom_horse_tack.block.entity;
 
 import com.outrightwings.truly_custom_horse_tack.block.ModBlocks;
+import com.outrightwings.truly_custom_horse_tack.item.CustomTackItem;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class SaddlerRackBlockEntity extends SingleInventoryBlockEntity {
     public SaddlerRackBlockEntity(BlockPos pos, BlockState state) {
         super(ModBlocks.SADDLE_RACK_BE.get(), pos, state);
+    }
+
+    @Override
+    public boolean validItem(ItemStack item) {
+        return item.getItem() instanceof CustomTackItem;
     }
 }
