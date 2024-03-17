1baea0f2d1c34d2ebe8381550b0d2b6ead65620b
lilym <lilymason@msn.com>
2024-03-16 22:33:04 -0700

    RGB Values Tooltips

--- src/main/java/com/outrightwings/truly_custom_horse_tack/item/CustomTackItem.java
+++ src/main/java/com/outrightwings/truly_custom_horse_tack/item/CustomTackItem.java
@@ -5,6 +5,7 @@ import com.outrightwings.truly_custom_horse_tack.item.tack.TackPattern;
 import net.minecraft.ChatFormatting;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Tuple;
 import net.minecraft.world.InteractionResult;
@@ -50,11 +51,19 @@ public class CustomTackItem extends HorseArmorItem {
         CompoundTag tagData =  stack.getTag();
         for(int i = 0; i < TackPattern.getPatternListSize(tagData); i++) {
             Tuple<Integer, String> colorPattern = TackPattern.getColorAndPatternByIndex(tagData, i);
-            var colorName = DyeColor.byId(colorPattern.getA()).getName();
-            var pattern = TackPattern.getTackPattern(colorPattern.getB());
-            var patternName = pattern != null ? pattern.getSerializedName() : "unknown";
 
-            list.add(Component.translatable(  String.format("tooltip.%s.%s.%s", Main.MODID,colorName,patternName)).withStyle(ChatFormatting.GRAY));
+            var pattern = TackPattern.getTackPattern(colorPattern.getB());
+            var patternName = pattern != null ? pattern.getTranslationKey() : "tack.truly_custom_horse_tack.pattern.unknown";
+
+            MutableComponent color;
+            if(colorPattern.getA() < 16){
+                var colorName = DyeColor.byId(colorPattern.getA()).getName();
+                color = Component.translatable(String.format("tooltip.%s.%s", Main.MODID,colorName));
+            } else {
+                float[] rgb = TackPattern.getColorFromColorTag(colorPattern.getA());
+                color = Component.literal(String.format("%d %d %d",(int)(rgb[0]*255),(int)(rgb[1]*255),(int)(rgb[2]*255)));
+            }
+            list.add(color.append(" ").append(Component.translatable(patternName)).withStyle(ChatFormatting.GRAY));
         }
         if(TackPattern.getPatternListSize(tagData) <= 0){
             list.add(Component.translatable(String.format("tooltip.%s.%s", Main.MODID,"no_pattern")).withStyle(ChatFormatting.GRAY));
