db485113085f128da0463d75f118ab645b2279f8
lilym <lilymason@msn.com>
2024-04-21 21:17:39 -0700

    Unicorn Horn, and Flags

---
+++ src/main/java/com/outrightwings/truly_custom_horse_tack/client/renderer/model/SpecialTack/FlagModel.java
@@ -0,0 +1,72 @@
+package com.outrightwings.truly_custom_horse_tack.client.renderer.model.SpecialTack;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.model.Model;
+import net.minecraft.client.model.geom.ModelPart;
+import net.minecraft.client.model.geom.PartPose;
+import net.minecraft.client.model.geom.builders.*;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.blockentity.BannerRenderer;
+import net.minecraft.client.resources.model.ModelBakery;
+import net.minecraft.util.Mth;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.item.BannerItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.entity.BannerBlockEntity;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import org.joml.Quaternionf;
+import sekelsta.horse_colors.entity.AbstractHorseGenetic;
+
+@OnlyIn(Dist.CLIENT)
+public class FlagModel extends Model {
+    final ModelPart flag;
+    final ModelPart pole;
+    final ModelPart bar;
+    public FlagModel(ModelPart root) {
+        super(RenderType::entityCutoutNoCull);
+        this.flag = root.getChild("flag");
+        this.pole = root.getChild("pole");
+        this.bar = root.getChild("bar");
+    }
+    public static LayerDefinition createBodyLayer() {
+        MeshDefinition meshdefinition = new MeshDefinition();
+        PartDefinition partdefinition = meshdefinition.getRoot();
+        partdefinition.addOrReplaceChild("flag", CubeListBuilder.create().texOffs(0, 0).addBox(0.0F, 0.0F, -0.5F, 20.0F, 40.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(1.0F, -62.0F, -1.5F, 0.0F, 0.0F, 1.5708F));
+        partdefinition.addOrReplaceChild("pole", CubeListBuilder.create().texOffs(44, 0).addBox(8.0F, -12.0F, 7.0F, 2.0F, 42.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(8.0F, 8.0F, -8.0F, 0.0F, 0.0F, -3.1416F));
+        partdefinition.addOrReplaceChild("bar", CubeListBuilder.create().texOffs(0, 42).addBox(-19.0F, -32.0F, 7.0F, 20.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-32.0F, -23.0F, -8.0F, 0.0F, 0.0F, 1.5708F));
+        return LayerDefinition.create(meshdefinition, 64, 64);
+    }
+
+    public void renderOnHorse(ItemStack bannerItem, AbstractHorseGenetic entityIn, PoseStack poseStack, MultiBufferSource bufferSource, int light, int overlay, float ticks) {
+        poseStack.pushPose();
+        poseStack.mulPose(new Quaternionf(-0.03f,0.70f,-0.03,0.70f));
+
+        float between = entityIn.getStandAnim(ticks);
+        poseStack.translate(Mth.lerp(between,0.3f,0.3f-0.43f),Mth.lerp(between,-0.2f,-0.2f-0.56f),-0.35f);
+
+        poseStack.pushPose();
+        poseStack.scale(0.6666667F, 0.6666667F, 0.6666667F);
+        VertexConsumer vertexConsumer = ModelBakery.BANNER_BASE.buffer(bufferSource, RenderType::entitySolid);
+        this.pole.render(poseStack,vertexConsumer,light,overlay,1,1,1,1);
+        this.bar.render(poseStack,vertexConsumer,light,overlay,1,1,1,1);
+
+        long time = Minecraft.getInstance().level.getGameTime();
+        float f2 = ((float)Math.floorMod(time, 100L) + ticks) / 100.0F;
+        this.flag.xRot = (-0.0125F + 0.01F * Mth.cos(((float)Math.PI * 2F) * f2)) * (float)Math.PI;
+        this.flag.x = 0;
+        this.flag.y = -42.0F;
+        var patterns = BannerBlockEntity.createPatterns(((BannerItem)bannerItem.getItem()).getColor() ,BannerBlockEntity.getItemPatterns(bannerItem));
+        BannerRenderer.renderPatterns(poseStack, bufferSource, light, overlay, this.flag, ModelBakery.BANNER_BASE, true,patterns);
+        poseStack.popPose();
+        poseStack.popPose();
+    }
+
+    @Override
+    public void renderToBuffer(PoseStack poseStack, VertexConsumer vertexConsumer, int packedLight, int overlay, float red, float green, float blue, float alpha) {
+
+    }
+}
---
+++ src/main/java/com/outrightwings/truly_custom_horse_tack/client/renderer/model/SpecialTack/HornModel.java
@@ -0,0 +1,85 @@
+package com.outrightwings.truly_custom_horse_tack.client.renderer.model.SpecialTack;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.outrightwings.truly_custom_horse_tack.Main;
+import net.minecraft.client.model.Model;
+import net.minecraft.client.model.geom.ModelPart;
+import net.minecraft.client.model.geom.PartPose;
+import net.minecraft.client.model.geom.builders.*;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import sekelsta.horse_colors.client.renderer.HorseGeneticModel;
+import sekelsta.horse_colors.entity.AbstractHorseGenetic;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+@OnlyIn(Dist.CLIENT)
+public class HornModel extends Model {
+    final ModelPart horn;
+    private static final ResourceLocation texture = new ResourceLocation(Main.MODID,"textures/entity/horse/special_tack/horn.png");
+
+    public HornModel(ModelPart root) {
+        super(RenderType::entityCutoutNoCull);
+        horn = root.getChild("horn");
+    }
+
+    public static LayerDefinition createBodyLayer() {
+        MeshDefinition meshdefinition = new MeshDefinition();
+        PartDefinition partdefinition = meshdefinition.getRoot();
+        // -0.5F, -8.5F, -2.5F
+        PartDefinition horn = partdefinition.addOrReplaceChild("horn", CubeListBuilder.create().texOffs(0, 0).addBox(-0.5F, -15.0F, 2.5F, 1.0F, 5.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 4.0F, -10.0F, 0.5235988F, 0.0F, 0.0F));
+
+        return LayerDefinition.create(meshdefinition, 16, 16);
+    }
+    public void renderOnHorse(AbstractHorseGenetic entityIn, PoseStack poseStack, MultiBufferSource bufferSource, int light, int overlay,float ticks,float limbSwing,float limbSwingAmount) {
+
+        float bodyRotation = this.updateHorseRotation(entityIn.yBodyRotO, entityIn.yBodyRot, ticks);
+        float headRotation = this.updateHorseRotation(entityIn.yHeadRotO, entityIn.yHeadRot, ticks);
+        float interpolatedPitch = entityIn.xRotO + (entityIn.getXRot() - entityIn.xRotO) * ticks;
+        float f4 = interpolatedPitch * 0.017453292F;
+        if (limbSwingAmount > 0.2F)
+        {
+            f4 += Mth.cos(limbSwing * 0.4F) * 0.15F * limbSwingAmount;
+        }
+
+        float headRelativeRotation = headRotation - bodyRotation;
+        headRelativeRotation = headRelativeRotation > 20.0F ? 20 : headRelativeRotation;
+        headRelativeRotation = headRelativeRotation < -20.0F ? -20 : headRelativeRotation;
+
+        float grassEatingAmount = entityIn.getEatAnim(ticks);
+        float rearingAmount = entityIn.getStandAnim(ticks);
+        float neckBend = rearingAmount + 1.0F - Math.max(rearingAmount, grassEatingAmount);
+
+        this.horn.setPos(0.0F, 4.0F, -10.0F);
+        this.horn.xRot = rearingAmount * (0.2617994F + f4) + grassEatingAmount * 2.1816616F + (1.0F - Math.max(rearingAmount, grassEatingAmount)) * 0.5235988F + f4;
+        this.horn.yRot = neckBend * headRelativeRotation * 0.017453292F;
+        this.horn.y = rearingAmount * -6.0F + grassEatingAmount * 11.0F + (1.0F - Math.max(rearingAmount, grassEatingAmount)) * 4.0F;
+        this.horn.z = rearingAmount * -1.0F + grassEatingAmount * -10.0F + (1.0F - Math.max(rearingAmount, grassEatingAmount)) * -10.0F;
+        System.out.println(String.format("y:%f z:%f",this.horn.y,this.horn.z));
+        VertexConsumer vertexConsumer = bufferSource.getBuffer(RenderType.entityCutoutNoCull(texture));
+        horn.render(poseStack,vertexConsumer,light,overlay);
+
+    }
+    @Override
+    public void renderToBuffer(PoseStack p_103111_, VertexConsumer p_103112_, int p_103113_, int p_103114_, float p_103115_, float p_103116_, float p_103117_, float p_103118_) {
+
+    }
+
+    private float updateHorseRotation(float prevRotation, float currentRotation, float partialTickTime) {
+        float bodyRotation;
+        for(bodyRotation = currentRotation - prevRotation; bodyRotation < -180.0F; bodyRotation += 360.0F) {
+        }
+
+        while(bodyRotation >= 180.0F) {
+            bodyRotation -= 360.0F;
+        }
+
+        return prevRotation + partialTickTime * bodyRotation;
+    }
+}
