b72bb4b444777c2f15782c59e98ec065ed55f965
lilym <lilymason@msn.com>
2024-04-27 02:18:42 -0700

    Horse Can Glide Terribly

--- src/main/java/com/outrightwings/truly_custom_horse_tack/mixin/AbstractHorseGeneticMixin.java
+++ src/main/java/com/outrightwings/truly_custom_horse_tack/mixin/AbstractHorseGeneticMixin.java
@@ -1,34 +1,67 @@
 package com.outrightwings.truly_custom_horse_tack.mixin;
 
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.animal.horse.AbstractHorse;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.BannerItem;
+import net.minecraft.world.item.ElytraItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import sekelsta.horse_colors.entity.AbstractHorseGenetic;
 
 @Mixin(AbstractHorseGenetic.class)
-public class AbstractHorseGeneticMixin extends Mob {
-    protected AbstractHorseGeneticMixin(EntityType<? extends Mob> p_21368_, Level p_21369_) {
-        super(p_21368_, p_21369_);
+public class AbstractHorseGeneticMixin extends AbstractHorse {
+
+
+    protected AbstractHorseGeneticMixin(EntityType<? extends AbstractHorse> p_30531_, Level p_30532_) {
+        super(p_30531_, p_30532_);
     }
 
     @Inject(method = "itemInteract(Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/InteractionHand;)Z", at = @At(value = "HEAD"),remap = false,cancellable = true)
     public void interact(Player player, ItemStack itemstack, InteractionHand hand, CallbackInfoReturnable<Boolean> cir){
         if(itemstack.getItem() instanceof BannerItem){
-            this.setItemSlot(EquipmentSlot.LEGS,itemstack.split(1));
+            this.setItemSlot(EquipmentSlot.FEET,itemstack.split(1));
             cir.setReturnValue(true);
         } else if (itemstack.is(Items.END_ROD)) {
             this.setItemSlot(EquipmentSlot.HEAD,itemstack.split(1));
             cir.setReturnValue(true);
+        } else if (itemstack.is(Items.ELYTRA)) {
+            this.setItemSlot(EquipmentSlot.LEGS,itemstack.split(1));
+            cir.setReturnValue(true);
         }
     }
+
+    public void updateFallFlying() {
+        boolean flag = false;
+        if (!this.onGround() && !this.isPassenger() && !this.hasEffect(MobEffects.LEVITATION)) {
+            ItemStack itemstack = this.getItemBySlot(EquipmentSlot.LEGS);
+            flag = itemstack.canElytraFly(this);// && itemstack.elytraFlightTick(this, this.fallFlyTicks);
+            if (!this.level().isClientSide) {
+                int nextFlightTick = this.fallFlyTicks + 1;
+                if (nextFlightTick % 10 == 0) {
+                    this.gameEvent(net.minecraft.world.level.gameevent.GameEvent.ELYTRA_GLIDE);
+                }
+            }
+        }
+
+        if (!this.level().isClientSide) {
+            this.setSharedFlag(7, flag);
+        }
+    }
+    /*protected Vec3 getRiddenInput(Player player, Vec3 vec) {
+
+    }*/
 }
