59e60967f3671a9e53560544ff72ccc9c825283d
lilym <lilymason@msn.com>
2024-05-12 22:04:20 -0700

    Rear Bells

---
+++ src/main/java/com/outrightwings/truly_custom_horse_tack/client/renderer/model/SpecialTack/BellsRearModel.java
@@ -0,0 +1,93 @@
+package com.outrightwings.truly_custom_horse_tack.client.renderer.model.SpecialTack;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.outrightwings.truly_custom_horse_tack.Main;
+import com.outrightwings.truly_custom_horse_tack.util.HorseModelRotations;
+import net.minecraft.client.model.geom.ModelPart;
+import net.minecraft.client.model.geom.PartPose;
+import net.minecraft.client.model.geom.builders.*;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import sekelsta.horse_colors.entity.AbstractHorseGenetic;
+
+public class BellsRearModel extends SpecialTackModel {
+    private final ModelPart bells;
+    private final ModelPart[] sideBells;
+    private static final ResourceLocation texture = new ResourceLocation(Main.MODID,"textures/entity/horse/special_tack/bell.png");
+
+    public BellsRearModel(ModelPart root) {
+        super(RenderType::entityCutoutNoCull);
+        this.bells = root.getChild("bells");
+        sideBells = new ModelPart[6];
+        sideBells[0] = bells.getChild("FW");
+        sideBells[4] = bells.getChild("FW2");
+        sideBells[1] = bells.getChild("BW");
+        sideBells[2] = bells.getChild("FE");
+        sideBells[5] = bells.getChild("FE2");
+        sideBells[3] = bells.getChild("BE");
+    }
+    public static LayerDefinition createBodyLayer() {
+        MeshDefinition meshdefinition = new MeshDefinition();
+        PartDefinition partdefinition = meshdefinition.getRoot();
+
+        PartDefinition bells = partdefinition.addOrReplaceChild("bells", CubeListBuilder.create(), PartPose.offset(-1f, 11.0F, 9.0F));
+
+        PartDefinition CE = bells.addOrReplaceChild("CE", CubeListBuilder.create().texOffs(0, 0).addBox(-0.6F, 0.5F, -1.0F, 1.0F, 2.0F, 2.0F, new CubeDeformation(0.0F))
+                .texOffs(0, 0).addBox(0.0F, 2.0F, -0.5F, 0.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-3.0F, -1.5F, 5.5F, 0.0F, -1.5708F, 0.0F));
+
+        PartDefinition CC = bells.addOrReplaceChild("CC", CubeListBuilder.create().texOffs(0, 0).addBox(-0.6F, 0.5F, -1.0F, 1.0F, 2.0F, 2.0F, new CubeDeformation(0.0F))
+                .texOffs(0, 0).addBox(0.0F, 2.0F, -0.5F, 0.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(1.0F, -0.5F, 5.5F, 0.0F, -1.5708F, 0.0F));
+
+        PartDefinition CW = bells.addOrReplaceChild("CW", CubeListBuilder.create().texOffs(0, 0).addBox(-0.6F, 0.5F, -1.0F, 1.0F, 2.0F, 2.0F, new CubeDeformation(0.0F))
+                .texOffs(0, 0).addBox(0.0F, 2.0F, -0.5F, 0.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(5.0F, -1.5F, 5.5F, 0.0F, -1.5708F, 0.0F));
+
+        PartDefinition FW2 = bells.addOrReplaceChild("FW2", CubeListBuilder.create().texOffs(0, 0).addBox(-0.4F, 0.0F, -1.0F, 1.0F, 2.0F, 2.0F, new CubeDeformation(0.0F))
+                .texOffs(0, 0).addBox(0.0F, 1.5F, -0.5F, 0.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(7.0F, -2.0F, 3.0F));
+
+        PartDefinition FW = bells.addOrReplaceChild("FW", CubeListBuilder.create().texOffs(0, 0).addBox(-0.4F, 0.0F, -1.0F, 1.0F, 2.0F, 2.0F, new CubeDeformation(0.0F))
+                .texOffs(0, 0).addBox(0.0F, 1.5F, -0.5F, 0.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(6.5F, -4.1F, -1.0F));
+
+        PartDefinition BW = bells.addOrReplaceChild("BW", CubeListBuilder.create().texOffs(0, 0).addBox(-0.4F, 0.0F, -1.0F, 1.0F, 2.0F, 2.0F, new CubeDeformation(0.0F))
+                .texOffs(0, 0).addBox(0.0F, 1.5F, -0.5F, 0.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(6.5F, -6.0F, -5.0F));
+
+        PartDefinition FE2 = bells.addOrReplaceChild("FE2", CubeListBuilder.create().texOffs(0, 0).addBox(-0.6F, 0.0F, -1.0F, 1.0F, 2.0F, 2.0F, new CubeDeformation(0.0F))
+                .texOffs(0, 0).addBox(0.0F, 1.5F, -0.5F, 0.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(-5.0F, -2.0F, 3.0F));
+
+        PartDefinition FE = bells.addOrReplaceChild("FE", CubeListBuilder.create().texOffs(0, 0).addBox(-0.6F, 0.0F, -1.0F, 1.0F, 2.0F, 2.0F, new CubeDeformation(0.0F))
+                .texOffs(0, 0).addBox(0.0F, 1.5F, -0.5F, 0.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(-4.5F, -4.1F, -1.0F));
+
+        PartDefinition BE = bells.addOrReplaceChild("BE", CubeListBuilder.create().texOffs(0, 0).addBox(-0.6F, 0.0F, -1.0F, 1.0F, 2.0F, 2.0F, new CubeDeformation(0.0F))
+                .texOffs(0, 0).addBox(0.0F, 1.5F, -0.5F, 0.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(-4.5F, -6.0F, -5.0F));
+
+        return LayerDefinition.create(meshdefinition, 16, 16);
+    }
+    public void renderOnHorse(AbstractHorseGenetic entityIn, PoseStack poseStack, MultiBufferSource bufferSource, int light, int overlay, float ticks, float limbSwing, float limbSwingAmount, float[] color) {
+        HorseModelRotations.rotateModelWithBody(bells,entityIn,ticks,limbSwing,limbSwingAmount);
+        float rearingAmount = entityIn.getStandAnim(ticks) * -0.7853982F;
+        for(ModelPart sideBell : sideBells) {
+            sideBell.xRot = -rearingAmount;
+        }
+
+        VertexConsumer vertexConsumer = bufferSource.getBuffer(RenderType.entityCutoutNoCull(texture));
+        bells.render(poseStack,vertexConsumer,light,overlay);
+    }
+
+    @Override
+    public void renderOnRack(BlockEntity blockEntity, PoseStack poseStack, MultiBufferSource bufferSource, int light, int overlay, boolean wall, float[] color) {
+        if(wall){
+            this.bells.setPos(-1,23,5);
+        }else{
+            this.bells.setPos(-1,17,6);
+        }
+        VertexConsumer vertexConsumer = bufferSource.getBuffer(RenderType.entityCutoutNoCull(texture));
+        bells.render(poseStack,vertexConsumer,light,overlay);
+    }
+
+    @Override
+    public void renderOnStand(BlockEntity blockEntity, PoseStack poseStack, MultiBufferSource bufferSource, int light, int overlay, boolean wall,float[] color) {
+
+    }
+}
