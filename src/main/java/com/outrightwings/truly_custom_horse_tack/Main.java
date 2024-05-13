be74393e0bfd41bbb9f85b52fd9ec20f9396d7a2
lilym <lilymason@msn.com>
2024-05-12 22:11:59 -0700

    rear circle

--- src/main/java/com/outrightwings/truly_custom_horse_tack/Main.java
+++ src/main/java/com/outrightwings/truly_custom_horse_tack/Main.java
@@ -55,7 +55,6 @@ public class Main
         Todo:
         * Bells
             * Make noise?
-            * Back bells
         * Shield banner textures
         * Wings in table
         * Add Horse Girls textures
