b811573c91a46444366279908bc236a1c34df264
lilym <lilymason@msn.com>
2024-09-03 16:34:36 -0700

    1.19 affied

--- src/main/java/com/outrightwings/truly_custom_horse_tack/item/recipe/TackMergeRecipe.java
+++ src/main/java/com/outrightwings/truly_custom_horse_tack/item/recipe/TackMergeRecipe.java
@@ -1,31 +1,23 @@
 package com.outrightwings.truly_custom_horse_tack.item.recipe;
 
 import com.outrightwings.truly_custom_horse_tack.item.CustomTackItem;
-import com.outrightwings.truly_custom_horse_tack.item.ModItems;
-import com.outrightwings.truly_custom_horse_tack.item.tack.TackPattern;
 import com.outrightwings.truly_custom_horse_tack.item.tack.TackTagUtility;
 import net.minecraft.core.NonNullList;
-import net.minecraft.core.RegistryAccess;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.nbt.ListTag;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Tuple;
 import net.minecraft.world.Container;
 import net.minecraft.world.inventory.CraftingContainer;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
-import net.minecraft.world.item.crafting.CraftingBookCategory;
 import net.minecraft.world.item.crafting.CustomRecipe;
 import net.minecraft.world.item.crafting.RecipeSerializer;
-import net.minecraft.world.item.crafting.SimpleCraftingRecipeSerializer;
 import net.minecraft.world.level.Level;
 
 public class TackMergeRecipe extends CustomRecipe {
-    public TackMergeRecipe(ResourceLocation resourceLocation, CraftingBookCategory bookCategory) {
-        super(resourceLocation,bookCategory);
+    public TackMergeRecipe(ResourceLocation resourceLocation) {
+        super(resourceLocation);
     }
 
-    Tuple<ItemStack,Integer> findDecorated(CraftingContainer container, int prev, boolean decorated){
+    Tuple<ItemStack,Integer> findDecorated(CraftingContainer container,int prev, boolean decorated){
         ItemStack found = null;
         int index = -1;
         for(int i = 0; i < container.getContainerSize(); i++){
@@ -58,7 +50,7 @@ public class TackMergeRecipe extends CustomRecipe {
     }
 
     @Override
-    public ItemStack assemble(CraftingContainer container, RegistryAccess access) {
+    public ItemStack assemble(CraftingContainer container) {
         var tackA = findDecorated(container, -100,true);
         var tackB = findDecorated(container, tackA.getB(),true);
         ItemStack tackWithPatterns = tackA.getA();
